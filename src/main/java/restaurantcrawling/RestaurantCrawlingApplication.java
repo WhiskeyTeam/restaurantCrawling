d555b3e49776cd68e2d2b600872ff12af97c6614
Lucas-Choi-17 <csissis@naver.com>
2024-08-07 21:19:22 +0900

    init: kakao api를 사용하여 데이터 추출해서 database에 입력하는 프로젝트 생성

---
+++ src/main/java/restaurantcrawling/RestaurantCrawlingApplication.java
@@ -0,0 +1,13 @@
+package restaurantcrawling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantCrawlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantCrawlingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/restaurantcrawling/RestaurantCrawlingApplicationTests.java
@@ -0,0 +1,13 @@
+package restaurantcrawling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantCrawlingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
